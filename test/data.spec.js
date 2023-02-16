cf12c9cebcfcc9f8753c76c7c126b155c0f64650
patriciadania <patricia.adania@hotmail.com>
2023-02-16 17:09:10 -0300

    Funções corretas e iniciando teste

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,23 +1,37 @@
-import { example, anotherExample } from '../src/data.js';
+import { buscaNome, buscaTag } from "./data.js";
 
-
-describe('example', () => {
-  it('is a function', () => {
-    expect(typeof example).toBe('function');
-  });
-
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
+describe("buscaNome", () => {
+  it("função de filtrar campeão por nome", () => {
+    const campeao = [
+      { name: "Aatrox" },
+      { name: "Morgana" },
+      { name: "Annie" },
+    ];
+    const nome = ["aa", "mor", "ann"];
+    const resultadoEsperado = [
+      {
+        campeao,
+      },
+    ];
+    expect(buscaNome(campeao, nome)).toBe(resultadoEsperado);
   });
 });
+it("retornar campeão digitado", () => {
+  expect(buscaNome("Aatrox", "Morgana", "Annie")).toBe("aa", "mor", "ann");
+});
 
-
-describe('anotherExample', () => {
-  it('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
+describe("buscaTag", () => {
+  it("função de filtrar campeão pelo tipo", () => {
+    const campeao = [{ name: "Shaco" }];
+    const tag = [{ tags: "Assassin" }];
+    const resultadoEsperado = [
+      {
+        campeao,
+      },
+    ];
+    expect(buscaTag(campeao, tag)).toBe(resultadoEsperado);
   });
-
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
-  });
+});
+it("retornar tipo selecionado", () => {
+  expect(buscaTag("Shaco")).toBe("Assassin");
 });
